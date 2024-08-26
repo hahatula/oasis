66bbaf3214c7cdd7ed3299be742a1f240f63a8b8
Olga Golubev <olgagolubev.pro@gmail.com>
2024-08-26 08:52:02 +0300

    add link to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,18 @@ import Toolbar from '../Toolbar/Toolbar';
 function Footer() {
   return (
     <div className="footer">
-      <p className='footer__info'>Developed by Olga Golubev</p>
+      <p className="footer__info">
+        Developed by{' '}
+        <a
+          href="https://www.linkedin.com/in/olgagolubev/"
+          target="_blank"
+          className="footer__link"
+        >
+          Olga Golubev
+        </a>
+      </p>
       <Toolbar />
-      <p className='footer__info footer__year'>2024</p>
+      <p className="footer__info footer__year">2024</p>
     </div>
   );
 }
