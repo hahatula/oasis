5b821ea43d6a9bd0ee0ba682c079cef6772fa444
Olga Golubev <olgagolubev.pro@gmail.com>
2024-08-31 10:17:20 +0300

    form select styling

--- src/components/ModalWithForm/ModalForm.tsx
+++ src/components/ModalWithForm/ModalForm.tsx
@@ -1,6 +1,7 @@
 import './ModalForm.css';
 import { Modal } from '../Modal/Modal';
 import { users } from '../../utils/tempDB';
+import { useState } from 'react';
 
 type ModalFormProps = {
   formName: string;
@@ -9,21 +10,65 @@ type ModalFormProps = {
 };
 
 function ModalForm({ formName, onClose, userId }: ModalFormProps) {
-    console.log({ formName, onClose, userId });
+  console.log({ formName, onClose, userId });
+  const [selectedResident, setSelectedResident] = useState({
+    id: 0,
+    name: 'Choose your resident',
+  });
+  const [optionsVisibility, setOptionsVisibility] = useState('hidden');
+
+  const toggleOptionsVisibility = () =>
+    optionsVisibility === 'shown'
+      ? setOptionsVisibility('hidden')
+      : setOptionsVisibility('shown');
+
+  //   const handleOptionClick = (resident) => {
+  //     setSelectedResident(resident);
+  //     toggleOptionsVisibility();
+  //   };
+
   return (
     <Modal name={formName} onClose={onClose}>
       <h2 className="form-title">New post</h2>
       <form action="submit" method="post" id={formName} className="form">
-        <select className="form__select" name="resident" id="resident" required>
-          <option value="">Choose your resident</option>
-          {
-            /* map all the residents of the current user */
-            users[userId-1].residents?.map((resident, index) => (
-              <option value={resident.id} key={index}>{resident.name}</option>
-            ))
-          }
-          <option value="">Add new resident</option>
-        </select>
+        <div className="form__select-wrapper">
+          <select
+            className="form__select"
+            name="resident"
+            id="resident"
+            required
+            onClick={toggleOptionsVisibility}
+          >
+            <option
+              className="form__select-default-option"
+              value={selectedResident.id}
+            >
+              {selectedResident.name}
+            </option>
+          </select>
+          <div className={`form__select-options_${optionsVisibility}`}>
+            {
+              /* map all the residents of the current user */
+              users[userId - 1].residents?.map((resident, index) => (
+                <div
+                  className="form__select-option"
+                  key={index}
+                  onClick={() => {
+                    setSelectedResident(resident);
+                    toggleOptionsVisibility();
+                  }}
+                >
+                  <img
+                    className="form__select-option-img"
+                    src={resident.avatarUrl}
+                    alt={resident.name}
+                  />
+                  {resident.name}
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </form>
     </Modal>
   );
